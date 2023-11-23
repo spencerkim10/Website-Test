0c47dc09ec473bd86dd64aff89b0ca674bd64f23
spencerkim10 <spencer.a.kim@gmail.com>
2023-11-23 00:57:35 -0500

    Invoice Table Model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Invoice(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    date = models.CharField(primary_key=False, max_length=8)
+    client = models.CharField(primary_key=False, max_length=100)
+    dollar = models.FloatField()
+    serial = models.CharField(primary_key=False, max_length=100)
+    truck = models.CharField(primary_key=False, max_length=100)
+
+    def __str__(self):
+        return self.id
